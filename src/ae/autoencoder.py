f99aea1d75e663d36a31c9d77bbbecb23de4d237
Devin Kwok <devin@devinkwok.com>
2020-06-03 18:20:24 -0600

    ADD dropout to inputs, ran experiment with 0.2 dropout.
    TODO: hidden layer noise
    TODO: testing on missing values in input

--- src/ae/autoencoder.py
+++ src/ae/autoencoder.py
@@ -22,60 +22,51 @@ if debug:
 # where n is number of subsequences per batch, N_BASE is number of channels
 # and subseq_len is the length of subsequences
 
+
+# this layer drops out every channel at some positions, and keeps inputs at size 1
+class SeqDropout(nn.Module):
+
+    def __init__(self, dropout_freq):
+        super().__init__()
+        self.dropout = nn.Dropout2d(dropout_freq)
+
+    def forward(self, x):
+        y = self.dropout(x.permute(0, 2, 1)).permute(0, 2, 1)
+        return y * (1 - self.dropout.p)
+
+
 class Autoencoder(nn.Module):
 
 
     def __init__(self, window_len=1, latent_len=1,
-                seq_len=2, seq_per_batch=1, empty_cutoff_prob=(1 / N_BASE)):
+                seq_len=2, seq_per_batch=1, dropout_freq=0.0):
         super().__init__()
         self.window_len = window_len
         self.latent_len = latent_len
         self.seq_len = seq_len
         self.seq_per_batch = seq_per_batch
-        self.empty_cutoff_prob = empty_cutoff_prob  # if no output is above this cutoff, predict empty (none)
-        self.encode_layer1 = nn.Conv1d(N_BASE, latent_len, window_len)
-        # need ConvTranspose to do deconvolution, otherwise channels go to wrong dimension
-        self.decode_layer1 = nn.ConvTranspose1d(latent_len, N_BASE, window_len)
+        self.dropout_freq = dropout_freq
+
         self.total_epochs = 0
-        # for convenience, length of the sequence which is decoded by window
-        self.decodable_len = self.seq_len - (self.window_len - 1)
 
+        encode_layers = []
+        if self.dropout_freq > 0:
+            encode_layers.append(SeqDropout(self.dropout_freq))
+        encode_layers.append(nn.Conv1d(N_BASE, latent_len, window_len))
+        encode_layers.append(nn.ReLU())
+        self.encode = nn.Sequential(*encode_layers)
 
-    def encode(self, x):
-        y = self.encode_layer1(x)
-        y = F.relu(y)
-        return y
-
-
-    def decode(self, y):
-        z = self.decode_layer1(y)
-        z = F.softmax(z, dim=1)
-        return z
+        decode_layers = []
+        # need ConvTranspose to do deconvolution, otherwise channels go to wrong dimension
+        decode_layers.append(nn.ConvTranspose1d(latent_len, N_BASE, window_len))
+        decode_layers.append(nn.Softmax(dim=1))
+        self.decode = nn.Sequential(*decode_layers)
 
 
     def forward(self, x):
         latent = self.encode(x)
         reconstructed = self.decode(latent)
         return reconstructed
-
-
-    def predict(self, reconstruction):
-        probabilities, indexes = torch.max(reconstruction, 1, keepdim=False)
-        indexes[(probabilities <= self.empty_cutoff_prob)] = N_BASE
-        output = F.one_hot(indexes, num_classes=N_BASE + 1)
-        output = output[:,:,:-1]
-        return output.permute(0, 2, 1)
-
-
-    def evaluate(self, x, true_x):
-        with torch.no_grad():
-            z = self.forward(x)
-            predictions = self.predict(z)
-            correct = (data_in.one_hot_to_seq(true_x) == data_in.one_hot_to_seq(predictions))
-            accuracy = torch.sum(correct).item() / correct.nelement()
-            error_indexes = torch.nonzero(torch.logical_not(correct), as_tuple=True)
-            #FIXME accuracy doesn't seem to be on each input separately
-        return accuracy, error_indexes
 
 
 def load_data(model, input_path, split_prop):
@@ -90,6 +81,25 @@ def load_data(model, input_path, split_prop):
     return train_loader, valid_loader
 
 
+# if no output is above the cutoff, predict empty (none)
+def predict(reconstruction, empty_cutoff_prob=(1 / N_BASE)):
+    probabilities, indexes = torch.max(reconstruction, 1, keepdim=False)
+    indexes[(probabilities <= empty_cutoff_prob)] = N_BASE
+    output = F.one_hot(indexes, num_classes=N_BASE + 1)
+    output = output[:,:,:-1]
+    return output.permute(0, 2, 1)
+
+
+def evaluate(model, x, true_x):
+    z = model.forward(x)
+    predictions = predict(z)
+    correct = (data_in.one_hot_to_seq(true_x) == data_in.one_hot_to_seq(predictions))
+    accuracy = torch.sum(correct).item() / correct.nelement()
+    error_indexes = torch.nonzero(torch.logical_not(correct), as_tuple=True)
+    #FIXME accuracy doesn't seem to be on each input separately
+    return accuracy, error_indexes
+
+
 def train(model, train_loader, valid_loader, optimizer, loss_fn, epochs):
     # target CPU or GPU
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -98,6 +108,7 @@ def train(model, train_loader, valid_loader, optimizer, loss_fn, epochs):
     validation = iter(valid_loader)
 
     for i in range(epochs):
+        model.train()
         model.total_epochs += 1
         loss_sum = 0
         for x, true_x in train_loader:
@@ -108,18 +119,19 @@ def train(model, train_loader, valid_loader, optimizer, loss_fn, epochs):
             loss.backward()
             optimizer.step()
 
-        accuracy, _ = model.evaluate(*next(validation))
+        model.eval()
+        accuracy, _ = evaluate(model, *next(validation))
         print("epoch {}, avg loss {}, validation acc. {}".format(
                 i, loss_sum / len(train_loader), accuracy))
 
 
 def run(hparams):
-    model = Autoencoder(hparams['window_len'], hparams['latent_len'],
-                hparams['seq_len'], hparams['seq_per_batch'], hparams['empty_cutoff_prob'])
+    model = Autoencoder(hparams['window_len'], hparams['latent_len'], hparams['seq_len'],
+            hparams['seq_per_batch'], hparams['dropout_freq'])
     train_loader, valid_loader = load_data(model, hparams['input_path'], hparams['split_prop'])
 
-    model_str = "{}x{}_{}_at{}".format(
-            model.window_len, model.latent_len,
+    model_str = "{}x{}_drop{}_{}_at{}".format(
+            model.window_len, model.latent_len, model.dropout_freq,
             model.total_epochs + hparams['epochs'], hparams['learn_rate'])
     print("Training {} for {} epochs".format(model_str, hparams['epochs']))
 
@@ -128,22 +140,23 @@ def run(hparams):
     loss_fn = nn.MSELoss()
     train(model, train_loader, valid_loader, optimizer, loss_fn, hparams['epochs'])
 
-    out_file = seq_util.io.output_path("ae01_", hparams['input_path'], model_str + '.pth')
+    out_file = seq_util.io.output_path(hparams['name'], hparams['input_path'], model_str + '.pth')
     print("Saving model to {}".format(out_file))
     torch.save(model.state_dict(), out_file)
 
 
 def experiment_1():
     hparams = {
+        'name': "ae01drop",
         'window_len': 1,
         'latent_len': 1,
         'seq_len': 32,
         'seq_per_batch': 4,
-        'empty_cutoff_prob': 0.25,
         'input_path': "data/ref_genome/test.fasta",
         'split_prop': 0.2,
         'epochs': 20,
         'learn_rate': 0.01,
+        'dropout_freq': 0.2,
     }
     # one latent var isn't enough, >= 2 bits should capture all 4 bp
     run(hparams)
@@ -174,4 +187,3 @@ def experiment_1():
 
 if __name__ == '__main__':
     experiment_1()
-    # ae = torch.load('outputs/src/ae/autoencoder/ae01_test1x4_20_at0.01.pth')
--- src/ae/test_autoencoder.py
+++ src/ae/test_autoencoder.py
@@ -33,7 +33,7 @@ class Test_Autoencoder(unittest.TestCase):
         for x, x_true in self.train_loader:
             latent = self.ae.encode(x)
             reconstructed = self.ae.decode(latent)
-            latent_shape = (self.ae.seq_per_batch, self.ae.latent_len, self.ae.decodable_len)
+            latent_shape = (self.ae.seq_per_batch, self.ae.latent_len, self.ae.seq_len - (self.ae.window_len - 1))
             self.assertEqual(latent.shape, latent_shape)
             self.assertEqual(reconstructed.shape, x.shape)
             break  # only test the first batch
@@ -41,42 +41,55 @@ class Test_Autoencoder(unittest.TestCase):
 
     def test_predict(self):
         for x, x_true in self.train_loader:
-            self.ae.empty_cutoff_prob = 0.0
-            seq = self.ae.predict(x)
+            seq = predict(x)
             npt.assert_array_equal(seq, x)
-            seq = self.ae.predict(self.ae.forward(x))
+            seq = predict(self.ae.forward(x))
             npt.assert_array_equal(torch.sum(seq, dim=1), torch.ones(x.shape[0], x.shape[2]))
-            self.ae.empty_cutoff_prob = 1.0
-            seq = self.ae.predict(self.ae.forward(x))
-            npt.assert_array_equal(torch.sum(seq, dim=1), torch.zeros(x.shape[0], x.shape[2]))
             break  # only test the first batch
 
 
     def test_evaluate(self):
         for x, x_true in self.train_loader:
-            accuracy, error_indexes = self.ae.evaluate(x, self.ae.predict(self.ae.forward(x)))
+            self.ae.eval()
+            accuracy, error_indexes = evaluate(self.ae, x, predict(self.ae.forward(x)))
             self.assertAlmostEqual(accuracy, 1.0)
             self.assertEqual(x[error_indexes[0],:, error_indexes[1]].nelement(), 0)
 
-            self.ae.empty_cutoff_prob = 0.0
-            accuracy, error_indexes = self.ae.evaluate(x, data_in.complement(
-                self.ae.predict(self.ae.forward(x))))
+            accuracy, error_indexes = evaluate(self.ae, x, data_in.complement(
+                predict(self.ae.forward(x))))
             flattened = x.permute(0, 2, 1).reshape((x.shape[0]*x.shape[2], x.shape[1]))
             self.assertAlmostEqual(accuracy, 0)
             npt.assert_array_equal(flattened, x[error_indexes[0],:, error_indexes[1]])
 
-            one_error = self.ae.predict(self.ae.forward(x))
+            one_error = predict(self.ae.forward(x))
             index_0, index_1 = 0, 1
-            if (one_error[index_0, 0, index_1] == 1):
-                one_error[index_0, 1, index_1] = 1
-            else:
-                one_error[index_0, 0, index_1] = 1
-            accuracy, error_indexes = self.ae.evaluate(x, one_error)
+            error = one_error[index_0, :, index_1].clone().detach()
+            one_error[index_0, :, index_1] = error.flip(0)
+            accuracy, error_indexes = evaluate(self.ae, x, one_error)
             self.assertLess(accuracy, 1.0)
             self.assertEqual(error_indexes[0], index_0)
             self.assertEqual(error_indexes[1], index_1)
 
             break  # only test the first batch
+
+
+    def test_dropout(self):
+        shape = (5, 4, 3)
+        ones = torch.ones(*shape)
+        zeros = torch.zeros(*shape)
+
+        dropout = SeqDropout(0.0)
+        npt.assert_array_equal(dropout(ones), ones)
+
+        dropout = SeqDropout(1.0)
+        npt.assert_array_equal(dropout(ones), zeros)
+
+        dropout = SeqDropout(0.5)
+        self.assertLess(torch.sum(dropout(ones)), torch.sum(ones))
+        self.assertGreater(torch.sum(dropout(ones)), 0)
+        position_wise_sum = torch.sum(dropout(ones), dim=1)
+        sums = torch.logical_or(position_wise_sum == 0., position_wise_sum == 4.)
+        self.assertTrue(torch.all(sums))
 
 
 if __name__ == '__main__':
